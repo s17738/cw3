7d38ce05fa27eb54ee6de13dce7ed1c678dc219a
s17738 <61625374+s17738@users.noreply.github.com>
2020-03-31 15:13:03 +0200

    Zadanie 7

--- s17738_cw3/s17738_cw3/Controllers/StudentsController.cs
+++ s17738_cw3/s17738_cw3/Controllers/StudentsController.cs
@@ -36,8 +36,20 @@ namespace s17738_cw3.Controllers
         [HttpPost]
         public IActionResult CreateStudent(Student student)
         {
-            student.IndexNumber = $"s{ new Random().Next(1, 10000)}";
+            student.IndexNumber = $"s{ new Random().Next(1, 30000)}";
             return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student student)
+        {
+            return Ok($"Student {id} has been updated to {student.FirstName} {student.LastName}");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok($"Student {id} has been deleted");
         }
 
     }
