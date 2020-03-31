0031567b153fa0a1cddaeb747479807ba838f20b
s17738 <61625374+s17738@users.noreply.github.com>
2020-03-31 13:58:24 +0200

    Zadanie 3

---
+++ s17738_cw3/s17738_cw3/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace s17738_cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Nowak, Kowalski, Piotrowicz";
+        }
+
+    }
+}
